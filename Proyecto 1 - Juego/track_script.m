539924b178e93023f1f8f2eeeb493a7075c4b1b0
Juan David Sánchez Jaramillo <jdavid.sanchez@udea.edu.co>
2018-09-11 01:46:53 -0500

    Script de reconocimiento del mayor objeto de un color específico con filtro optimizado, reconocimiento de dirección de movimiento, movimiento a través de cuadrículas del objeto, imágenes con figuras que muestran el punto donde se encuentra el objeto y el centro para mayor facilidad en el juego, fluidez mejorada, figuras de celdas, personaje y enemigo añadidos.

--- Proyecto 1 - Juego/track_script.m
+++ Proyecto 1 - Juego/track_script.m
@@ -1,19 +1,39 @@
+close all; clear all; clc;
+celdas = imread('celdas-2.0.png');
+[celdas_rows, celdas_cols, ~] = size(celdas);
+character = imread('character-2.0.png');
+[char_rows, char_cols, ~] = size(character);
+
+offset_x = char_cols-1;
+offset_y = char_rows-1;
+cel_char = celdas;
+
+limit_pos_x = celdas_cols/char_cols-1;
+limit_pos_y = celdas_rows/char_rows-1;
+
+pos_x = 0;
+pos_y = 0;
+cambio = 0;
+able = 0;
+
+cam = ipcam('http://192.168.1.12:8080/video');
+img = snapshot(cam);
+[rows, cols, depth] = size(img);
+
+center_x = cols/2;
+center_y = rows/2;
+offset_center_x = 120;
+offset_center_y = 100;
+
 while 1
     
-    img = imread('http://192.168.1.12:8080/shot.jpg');
-    [rows, columns, depth] = size(img);
-    for i = 1 : rows
-        for j = 1 :  columns
-            if  img(i,j,1) < 150 || img(i,j,2) > 100 || img(i,j,3) > 100
-                img(i,j,:) = 0;
-            end
-        end
-    end
-    diff_im = imsubtract(img(:,:,1), rgb2gray(img));
-    diff_im = medfilt2(diff_im,[3,3]);
-    diff_im = imbinarize(diff_im);
-    diff_im = bwareaopen(diff_im, 300);
-    [bw, lab_num] = bwlabel(diff_im, 4);
+    img = snapshot(cam);
+
+    imRed = img(:,:,1) > 130; imGreen = img(:,:,2) < 50; imBlue = img(:,:,3) < 50;
+    img_bin = and(and(imRed, imGreen), imBlue);
+    img_bin = bwareaopen(img_bin, 500);
+    
+    [bw, lab_num] = bwlabel(img_bin, 4);
     labels = zeros(1, lab_num);
     for i = 1 : lab_num
         labels(i) = sum(bw(:) == i);
@@ -26,52 +46,61 @@ while 1
         bw_clean(bw ~= max_ind) = 0;
         
         stats = regionprops(bw_clean, 'BoundingBox', 'Centroid');
-        
-        
-        figure(1); imshow(bw_clean);
-        
-        hold on
-        
-        bb = stats(object).BoundingBox;
-        bc = stats(object).Centroid;
-        
+        bb = stats(1).BoundingBox;
+        bc = stats(1).Centroid;
         current_coordinate_x = bc(1,1);
         current_coordinate_y = bc(1,2);
         
-        %{
-        if current_coordinate_x > 420 && able == 1
-            move_right = 1;
-            move_left = 0;
-            move_up = 0;
-            move_down = 0;
-        elseif current_coordinate_x < 300 && able == 1
-            move_right = 0;
-            move_left = 1;
-            move_up = 0;
-            move_down = 0;
-        elseif current_coordinate_y < 200 && able == 1
-            move_right = 0;
-            move_left = 0;
-            move_up = 1;
-            move_down = 0;
-        elseif current_coordinate_y > 320 && able == 1
-            move_right = 0;
-            move_left = 0;
-            move_up = 0;
-            move_down = 1;
-        else
-            move_right = 0;
-            move_left = 0;
-            move_up = 0;
-            move_down = 0;
+        centroid = zeros(rows, cols);
+        centroid = insertShape(centroid, 'FilledCircle',[current_coordinate_x,current_coordinate_y,40],'LineWidth',5,'Color', 'white');
+        
+        if (current_coordinate_x > center_x + offset_center_x) && able == 1 && pos_x < limit_pos_x
+            pos_x = pos_x + 1;
+            able = 0;
+            disp('Derecha');
+            cambio = 1;
+        elseif (current_coordinate_x < center_x - offset_center_x) && able == 1 && pos_x > 0
+            pos_x = pos_x - 1;
+            able = 0;
+            disp('Izquierda');
+            cambio = 1;
+        elseif (current_coordinate_y > center_y + offset_center_y) && able == 1 && pos_y < limit_pos_y
+            pos_y = pos_y + 1;
+            able = 0;
+            disp('Abajo');
+            cambio = 1;
+        elseif (current_coordinate_y < center_y - offset_center_y) && able == 1 && pos_y > 0
+            pos_y = pos_y - 1;
+            able = 0;
+            disp('Arriba');
+            cambio = 1;
+        elseif current_coordinate_x < center_x + offset_center_x && current_coordinate_x > center_x - offset_center_x && current_coordinate_y < center_y + offset_center_y && current_coordinate_y > center_y - offset_center_y
+            able = 1;
+            disp('Centro');
         end
-        %}
         
-        rectangle('Position', bb, 'EdgeColor', 'r', 'LineWidth', 2)
-        plot(bc(1), bc(2), '-m+')
+        if cambio == 1
+            cel_char = celdas;
+            for i = 1 : char_rows
+                for j = 1 : char_cols
+                    cel_char(i+(offset_y*pos_y), j+(offset_x*pos_x), :) = character(i,j, :);
+                end
+            end
+            cambio = 0;
+        end
         
-        hold off
+        if able == 1
+            object_and_center = insertShape(centroid,'Rectangle',[(center_x - offset_center_x) (center_y - offset_center_y) (offset_center_x*2) (offset_center_y*2)],'LineWidth',5,'Color','green');
+        else
+            object_and_center = insertShape(centroid,'Rectangle',[(center_x - offset_center_x) (center_y - offset_center_y) (offset_center_x*2) (offset_center_y*2)],'LineWidth',5,'Color','red');
+        end
+                
+        figure(1);
+        subplot(1,2,1);
+        imshow(cel_char);
         
+        subplot(1,2,2);
+        imshow(object_and_center);
     end
     
 end
